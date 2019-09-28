6092edaa514f708b229f0e99274769655492869f
Remzi <ict@remzi.info>
2019-09-28 20:57:37 +0200

    Improved assignment 1

--- src/transform.js
+++ src/transform.js
@@ -1,3 +1,15 @@
+/*
+
+1. You must create a module named transform that exports a function named groupAdultsByAgeRange. The test imports it like this:
+const groupAdultsByAgeRange = require("./transform").groupAdultsByAgeRange
+Note: This is a named export
+
+2. The exported groupAdultsByAgeRange should be a function.
+
+*/
+
+
+
 function groupAdultsByAgeRange() {
     
 }
